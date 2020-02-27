bf746e406e93e8dc55a119c9d992e1db83183ae3
ashishdroid <ashish08github@gmail.com>
2020-02-27 17:30:20 +0400

    career path (Akshay, radio button)

---
+++ app/src/main/java/com/ashishramkissoon/careerpath/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ashishramkissoon.careerpath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Utils.darkenStatusBar(this, R.color.blue);
+
+    }
+}
